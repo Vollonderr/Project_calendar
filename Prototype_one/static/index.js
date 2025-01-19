01474c2e265a4ea85a3bc77b68e22304bb1f5ba2
Vollonderrin <volloddimirr@gmail.com>
2025-01-19 19:44:37 +0300

    Add files via upload

--- Prototype_one/static/index.js
+++ Prototype_one/static/index.js
@@ -2,34 +2,33 @@ const vm = new Vue({
     el: '#vm',
     delimiters: ['[[', ']]'],
     data: () => ({
-      event_all: [],
+      events_count: 0,
       update_value: '',
-      importance_value: '0',
-      status_value: '0',
       importance_change: '',
+      status_change: '',
       valve_add: false,
       valve_date: false
     }),
     methods:{
       get_events: function(e) {
-        this.event_all = e.slice(0)
-        return this.event_all
+        return e.slice(0)
       },
 
       get_importance_value: function(e) {
-        this.importance_value = e
-        return this.importance_value
+        if(e === '') return '0'
+        return e
       },
 
       get_status_value: function(e) {
-        this.status_value = e
-        return this.status_value
+        if(e === '') return '0'
+        return e
       },
 
       get_valve_date: function(e) {
         if(e == 0) return true
         else return false
       }
+
     }
 })
 

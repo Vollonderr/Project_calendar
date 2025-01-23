85b2eb48893a6127e4cb93e3100215c0c49ff248
Vollonderrin <volloddimirr@gmail.com>
2025-01-23 22:36:56 +0300

    Add files via upload

--- Prototype_one/main.py
+++ Prototype_one/main.py
@@ -1,9 +1,10 @@
 from models import get_events, add_event, update_event, delete_event
 
 from datetime import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'JH_2tg-LxpEiSMFq3f8Cyey_h'
 
 @app.route('/add', methods=['POST'])
 def get_event_add():
@@ -19,15 +20,18 @@ def get_event_add():
 		return 'Заполнены не все поля!'
 	if end == '':
 		add_event(name, start, '0', supervisor, note, importance, status)
+		flash("Заметка \"" + name + "\" была успешна добавлена!")
 		return redirect("/")
 	if datetime.strptime(start, '%Y-%m-%d') > datetime.strptime(end, '%Y-%m-%d'):
 		return 'Дата начала срока должна быть после даты конца срока!'
 	add_event(name, start, end, supervisor, note, importance, status)
+	flash("Мероприятие \"" + name + "\" было успешно добавлено!")
 	return redirect("/")
 
 @app.route('/update', methods=['POST'])
 def get_event_update():
 	name = request.form['name']
+	realname = request.form['realname']
 	start = request.form['start']
 	end = request.form['end']
 	supervisor = request.form['supervisor']
@@ -41,16 +45,20 @@ def get_event_update():
 		return 'Заполнены не все поля!'
 	if end == '':
 		update_event(name, start, '0', supervisor, note, importance, status, ROWID)
+		flash("Мероприятие \"" + realname + "\" было успешно обновлено!")
 		return redirect("/")
 	if datetime.strptime(start, '%Y-%m-%d') > datetime.strptime(end, '%Y-%m-%d'):
 		return 'Дата начала срока должна быть после даты конца срока!'
 	update_event(name, start, end, supervisor, note, importance, status, ROWID)
+	flash("Мероприятие \"" + realname + "\" было успешно обновлено!")
 	return redirect("/")
 
 @app.route('/delete', methods=['POST'])
 def get_del():
 	ROWID = request.form['ROWID']
+	name = request.form['name']
 	delete_event(ROWID)
+	flash("Мероприятие \"" + name + "\" было успешно удалено!")
 	return redirect("/")
 
 @app.route('/')
